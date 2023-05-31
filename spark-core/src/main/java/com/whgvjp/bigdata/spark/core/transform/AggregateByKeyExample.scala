391ab53e206f9fe8f2861f295c32776e290d122f
whgvjp <834919155@qq.com>
2023-05-31 17:37:20 +0800

    AggregateByKey案例

--- spark-core/src/main/java/com/whgvjp/bigdata/spark/core/transform/AggregateByKeyExample.scala
+++ spark-core/src/main/java/com/whgvjp/bigdata/spark/core/transform/AggregateByKeyExample.scala
@@ -10,7 +10,7 @@ object AggregateByKeyExample {
     // 创建一个包含键值对的RDD
     val rdd = spark.sparkContext.parallelize(Seq(("A", 1), ("B", 2), ("A", 3), ("B", 4), ("C", 5)))
 
-    // 使用aggregateByKey计算每个键的总和和计数
+    // 使用aggregateByKey计算每个键的总和和计数，两组参数
     val resultRDD = rdd.aggregateByKey((0, 0))(
       (acc, value) => (acc._1 + value, acc._2 + 1),
       (acc1, acc2) => (acc1._1 + acc2._1, acc1._2 + acc2._2)
