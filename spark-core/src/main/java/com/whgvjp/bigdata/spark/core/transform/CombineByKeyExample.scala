0acc966ccc27425a970f0bc5c8e0b4658318ccc8
whgvjp <834919155@qq.com>
2023-06-01 10:23:13 +0800

    CombineByKey案例

--- spark-core/src/main/java/com/whgvjp/bigdata/spark/core/transform/CombineByKeyExample.scala
+++ spark-core/src/main/java/com/whgvjp/bigdata/spark/core/transform/CombineByKeyExample.scala
@@ -10,6 +10,12 @@ object CombineByKeyExample {
     val rdd = spark.sparkContext.parallelize(Seq(("A", 1), ("B", 2), ("A", 3), ("B", 4), ("C", 5)))
 
     // 使用combineByKey计算每个键的总和和计数
+    //该函数接受三个参数：
+    //
+    //createCombiner：一个函数，用于将每个键的第一个值转换为累加器的初始值。它将一个值 V 转换为累加器的类型 C。该函数在每个分区中的每个键的第一个值上调用，
+    // 用于初始化累加器。
+    //mergeValue：一个函数，用于将每个分区中的值与累加器进行合并。它接受两个参数，累加器的类型 C 和值 V，并返回一个合并后的累加器。
+    //mergeCombiners：一个函数，用于合并不同分区的累加器。它接受两个累加器 C，并返回一个合并后的累加器。
     val resultRDD = rdd.combineByKey(
       value => (value, 1),
       (acc: (Int, Int), value) => (acc._1 + value, acc._2 + 1),
